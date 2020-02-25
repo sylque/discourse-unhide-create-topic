754ce24075630f32ab8a3ba7dd6b69ba57b6fdfc
sylque <sylvain.quendez@gmail.com>
2020-02-25 14:33:23 +0100

    Added plugin url

--- plugin.rb
+++ plugin.rb
@@ -2,6 +2,7 @@
 # about: A Discourse plugin to unhide the Create Topic button
 # version: 1.0.0
 # authors: Sylvain Quendez
+# url: https://github.com/sylque/discourse-unhide-create-topic
 
 enabled_site_setting :discourse_unhide_create_topic_enabled
 
